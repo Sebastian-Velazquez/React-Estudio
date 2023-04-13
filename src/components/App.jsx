992418a9c75155b36ba6345869351f580550d387
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-04-13 13:52:30 -0300

    asd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,15 @@ import '../assets/css/App.css';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
+import Props from './Props';
 
 function App() {
   return (//solo se puede devolver un elemento html. por eso guardarun una sola etiqueta
     <React.StrictMode>
       <Header/>
       <Body/>
+      <Props generos={["Fantasa", "Aventura"]} rating="8.0" tiulo="Harry Potter"/>
+      <Props generos={["Fantasa", "Aventura"]}  tiulo="Harry Potter"/>
       <Footer/>
     </React.StrictMode>
   );
